ca48d94207d02436bf60b2bb0ca5eca402863dbb
Fabian David Dueñas <fabian0896@outlook.com>
2022-01-24 21:30:03 -0500

    [ADD] hide heder directive

--- src/app/directives/hide-header.directive.ts
+++ src/app/directives/hide-header.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Input, Renderer2 } from '@angular/core';
+import { DomController, ScrollCustomEvent } from '@ionic/angular';
 
 @Directive({
   selector: '[appHideHeader]'
 })
 export class HideHeaderDirective {
+  @Input('appHideHeader') header: any;
+  readonly maxLen = 100;
 
-  constructor() { }
+  constructor(private render: Renderer2, private domCrt: DomController) { }
+
+  @HostListener('ionScroll', ['$event'])
+  onScroll($event: ScrollCustomEvent) {
+    const scrollTop = $event.detail.scrollTop;
+
+    const opacityValue = Math.max(0, this.maxLen - scrollTop) / 100;
+    const topValue = Math.min(this.maxLen, scrollTop);
+
+    this.domCrt.write(() => {
+      this.render.setStyle(this.header, 'opacity', opacityValue);
+      this.render.setStyle(this.header, 'top', -topValue + 'px');
+    });
+  }
 
 }
--- src/app/pages/mail/mail.page.ts
+++ src/app/pages/mail/mail.page.ts
@@ -3,6 +3,10 @@ import { Component, OnInit } from '@angular/core';
 
 import { Mail } from 'src/app/models/mail.model';
 import randomColor from 'randomcolor';
+import { Router } from '@angular/router';
+import { PopoverController } from '@ionic/angular';
+import { AccountPage } from '../account/account.page';
+import { AccountPageModule } from '../account/account.module';
 
 @Component({
   selector: 'app-mail',
@@ -12,7 +16,11 @@ import randomColor from 'randomcolor';
 export class MailPage implements OnInit {
   emails: Mail[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private popoverCrt: PopoverController,
+  ) { }
 
   ngOnInit() {
     this.http.get<Mail[]>('./assets/data.json')
@@ -29,6 +37,19 @@ export class MailPage implements OnInit {
     setTimeout(() => {
       event.target.complete();
     }, 2000);
+  }
+
+  async openAccount(event) {
+    const popover = await this.popoverCrt.create({
+      component: AccountPage,
+      event,
+      cssClass: 'custom-popover',
+    });
+    await popover.present();
+  }
+
+  openDetails(id: string) {
+    this.router.navigate(['tabs', 'mail', id]);
   }
 
 }
